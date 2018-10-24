a5fea884bda9e319f56a140ea904a957bb1fca67
mahmudul <mahmudul2981@diu.edu.bd>
2018-10-24 17:43:41 +0600

    Initial commit

---
+++ app/src/main/java/gorental/com/day4ext3/MainActivity.java
@@ -0,0 +1,22 @@
+package gorental.com.day4ext3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button1, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+        button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.angry_btn);
+    }
+}
